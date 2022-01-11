2ce9f5f7350b247af41bf9f956385d2547d2bb9f
JazzPants <leighton.l14@gmail.com>
2022-01-12 10:49:07 +1100

    Done!

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,18 @@
+require 'pry'
+#import pry debugging
+
+# def plus_two(num)
+# 	num + 2
+# 	binding.pry
+# 	num
+# end
+
 def plus_two(num)
-	num + 2
-	num
+	result = num + 2
+	binding.pry
+	result
 end
+
+plus_two(3)
+
+#binding.pry is like "debugger" in javascript. pauses the method
